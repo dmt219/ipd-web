07cf9bf4daf07b165a14536346bf460f5e713448
dmt219 <minhduc20021997@gmail.com>
2018-07-24 13:53:16 +0900

    users management (not integrated with front end yet)

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 var config = require('./config'),
-mongoose = require('mongoose');
-module.exports = function() {
-var db = mongoose.connect(config.db);
-require('../app/models/users.server.models');
-return db;
+    mongoose = require('mongoose');
+module.exports = function () {
+    var db = mongoose.connect(config.db);
+    require('../app/models/user');
+    return db;
 };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var mongoose = require('./config/mongoose'),
-express = require('./config/express'),
-passport = require('./config/passport');
+    express = require('./config/express'),
+    passport = require('./config/passport');
 var db = mongoose();
 var app = express();
 var passport = passport();
